8aa39817219fa5a0927821b7c7814afbb7bbec84
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2024-08-21 18:57:43 +1000

    Update ParseEvent.php

--- src/skyss0fly/Skroll/Events/ParseEvent.php
+++ src/skyss0fly/Skroll/Events/ParseEvent.php
@@ -2,27 +2,30 @@
 namespace Skroll\ParseEvent;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
 use pocketmine\event\EventManager;
-use pocketmine\event\EventListener;
 
-final class ParseEvent extends PluginBase {
+final class ParseEvent extends PluginBase implements Listener {
 
-  public function errorHandlerEvent(Array $args ): void {
-    $lists = ["Error: A script is Unreadble due to A Syntax Error"];
+  public function errorHandlerEvent(array $args): void {
+    $lists = ["Error: A script is unreadable due to a syntax error"];
     
-$this->getLogger()->error($lists[$args]);
-    
+    if (isset($args[0]) && is_int($args[0]) && $args[0] < count($lists)) {
+      $this->getLogger()->error($lists[$args[0]]);
+    } else {
+      $this->getLogger()->error("Unknown error.");
+    }
   }
 
-  public function cancelErrorEvent() bool {
+  public function cancelErrorEvent(): bool {
+    // Assuming you have some logic to determine if the error can be canceled
+    $canCancel = true; // Replace this with your actual logic
 
-    if // error cannot be canceled then return false
-
-    else // return true;
-
-      
+    if ($canCancel) {
+      return true;
+    } else {
+      return false;
+    }
   }
-  
 }
-
 ?>
